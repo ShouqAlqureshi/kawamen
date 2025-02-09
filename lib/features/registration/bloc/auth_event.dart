b234f3705845afe135c00010a93d1653985ea585
Raghad <raghadalotibi939@gmail.com>
2025-02-09 20:52:48 +0300

    rest of the files

---
+++ lib/features/registration/bloc/auth_event.dart
@@ -0,0 +1,33 @@
+// features/registration/bloc/auth_event.dart
+import 'package:equatable/equatable.dart';
+
+abstract class AuthEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+// Event for user registration
+class RegisterUser extends AuthEvent {
+  final String fullName;
+  final String email;
+  final String password;
+
+  RegisterUser({required this.fullName, required this.email, required this.password});
+
+  @override
+  List<Object> get props => [fullName, email, password];
+}
+
+// Event for login
+class LoginUser extends AuthEvent {
+  final String email;
+  final String password;
+
+  LoginUser({required this.email, required this.password});
+
+  @override
+  List<Object> get props => [email, password];
+}
+
+// Event for logout
+class LogoutUser extends AuthEvent {}
