f9511c42b972696b0f4da3ccd521a44e3db3eb4b
Miriam Almogren <miriamoffic@gmail.com>
2025-02-11 09:53:07 +0300

    app theme

--- lib/core/app/app_view.dart
+++ lib/core/app/app_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:kawamen/core/navigation/app_routes.dart';
+import 'package:kawamen/core/utils/theme/theme.dart';
+import 'package:kawamen/features/Profile/Screens/view_profile_screen.dart';
+import 'package:kawamen/features/login/view/login_page.dart';
+import 'package:kawamen/features/registration/screens/registration_screen.dart';
 import 'package:kawamen/intro_screen.dart';
 
 class AppView extends StatelessWidget {
@@ -10,7 +14,7 @@ class AppView extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Kawamen',
-      theme: ThemeData.dark(),
+      theme: AppTheme.darkTheme,
       home: EntryScreen(),
     );
   }
