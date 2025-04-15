8b7522c25769bbbf04914b1ee7a0004bf557d120
ShouqAlqureshi <123020118+ShouqAlqureshi@users.noreply.github.com>
2025-04-15 12:28:20 +0300

    detection error solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kawamen/core/app/app.dart';
 import 'package:kawamen/features/Profile/repository/profile_repository.dart';
 import 'package:kawamen/features/registration/bloc/auth_bloc.dart';
@@ -12,10 +13,19 @@ import 'core/services/Notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+   try {
+    await dotenv.load(fileName: ".env");
+  } catch (e) {
+    print("Error loading .env file: $e");
+    // Provide fallback values or handle the error
+  }
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform, // If using Firebase CLI
+    options: DefaultFirebaseOptions.currentPlatform,
   );
-    await NotificationService().initialize();
-    listenForEmailVerification();  // Call this once at app startup
+  
+  await NotificationService().initialize();
+  listenForEmailVerification();  // Call this once at app startup
+  
   runApp(App());
 }
